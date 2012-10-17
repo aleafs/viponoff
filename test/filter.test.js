18babbf574e42dbe68a007c275ce5b40ae8ec636
aleafs <zhangxc83@gmail.com>
2012-10-17 14:39:43 +0800

    开发更新

--- test/filter.test.js
+++
@@ -1,6 +0,0 @@
-/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
-
-var should  = require('should');
-
-describe('', function () {
-});
---
+++ test/viponoff.test.js
@@ -0,0 +1,14 @@
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+
+var should  = require('should');
+var viponoff = require(__dirname + '/../');
+
+describe('viponoff test', function () {
+
+  /* {{{ should_work_with_http_server_works_fine() */
+  it ('should_work_with_http_server_works_fine', function (done) {
+    done();
+  });
+  /* }}} */
+
+});
