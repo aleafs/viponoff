361913fdfc60de4537edbeb09b7fb46596b4e1b2
aleafs <zhangxc83@gmail.com>
2012-10-17 15:06:40 +0800

    开发更新

--- test/viponoff.test.js
+++ test/viponoff.test.js
@@ -13,14 +13,28 @@ var filter  = require(__dirname + '/../').create({
 describe('viponoff test', function () {
 
   /* {{{ should_work_with_http_server_works_fine() */
-  it ('should_work_with_http_server_works_fine', function (done) {
+  it ('should_work_with_http_server_works_fine', function (_done) {
 
+    var n = 2;
     var s = require('http').createServer(filter).listen(8124);
+
+    var done  = function () {
+      if ((--n) === 0) {
+        s.close();
+        _done();
+      }
+    };
+
     urllib.request('http:/' + '/localhost:8124/aa', function (error, data, res) {
       data.toString().should.eql('hello world');
+      res.statusCode.should.eql(200);
       done();
     });
 
+    urllib.request('http:/' + '/localhost:8124/are_you_ok', function (error, data, res) {
+      res.statusCode.should.eql(404);
+      done();
+    });
   });
   /* }}} */
 
